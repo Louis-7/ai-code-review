df84be84ed19b0217f4095c571cc158acb08e505
Louis-7 <louisgh.cn@gmail.com>
2023-04-29 11:50:20 +0800

    feat: set max file/patch number

--- action/core/code-review.d.ts
+++ action/core/code-review.d.ts
@@ -2,9 +2,15 @@ import { Context } from "probot";
 import { OpenAI } from './openai-helper';
 export declare class CodeReview {
     PATH_TO_EXCLUDE: string;
+    MAX_FILE_PER_PR: number;
+    MAX_PATCH_PER_FILE: number;
     openai: OpenAI;
     constructor();
     review(context: Context<'pull_request.opened' | 'pull_request.synchronize'>): Promise<void>;
     private generatePrompt;
     private isExcluded;
+    private preProcessPullRequestContext;
+    private getDiffFiles;
+    private getCodeReviewResponseFromAI;
+    private addCodeReviewToPullRequest;
 }
