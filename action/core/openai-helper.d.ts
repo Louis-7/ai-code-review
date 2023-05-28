3cea07800fe43233f6864ae9b0ede85d134f0403
Louis-7 <louisgh.cn@gmail.com>
2023-05-28 13:50:49 +0800

    feat: update action

--- action/core/openai-helper.d.ts
+++ action/core/openai-helper.d.ts
@@ -1,6 +1,9 @@
 import { Configuration, OpenAIApi } from 'openai';
 export declare class OpenAI {
     OPENAI_API_KEY: string;
+    MODEL: string;
+    TEMPERATURE: number;
+    TOP_P: number;
     configuration: Configuration;
     openai: OpenAIApi;
     constructor();
