c35e91edb0617e1cb0fd824d2beb248fcb9e9896
Louis-7 <louisgh.cn@gmail.com>
2023-07-22 11:00:27 +0800

    ci: add debug log

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,13 @@ export = (app: Probot) => {
       default:
         break;
     }
+
+    console.log('PATH_TO_EXCLUDE: ', process.env.PATH_TO_EXCLUDE);
+    console.log('MAX_FILE_PER_PR: ', process.env.MAX_FILE_PER_PR);
+    console.log('MAX_PATCH_PER_FILE: ', process.env.MAX_PATCH_PER_FILE);
+    console.log('LANGUAGE: ', process.env.LANGUAGE);
+    console.log('CUSTOMIZED_PROMPT: ', process.env.CUSTOMIZED_PROMPT);  
+    
     const codeReview = new CodeReview();
     await codeReview.review(context as any);
   });
